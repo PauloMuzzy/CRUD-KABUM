fc0d6d1a6ae1dcdb1d2d45bbbf647c8cbc218384
PauloMuzzy <paulokriger@gmail.com>
2021-11-12 13:22:17 -0300

    add lógica de login sem back-end

--- www/scriptIndex.js
+++ www/scriptIndex.js
@@ -1,5 +1,27 @@
 $(document).ready(function () {
 
+    function verificaLogin() {
+
+        var pegarLogin = localStorage.getItem("login");
+        var pegarSenha = localStorage.getItem("senha");
+
+        $("#loginCliente").hide();
+        // validação sem back-end
+        if (pegarLogin == "paulo muzzy" && pegarSenha == "123123") {
+            console.log("LOGADO");
+            var chaveLogado = "1";
+            localStorage.setItem("estaLogado", chaveLogado);
+            $("#loginCliente").show();
+        } else {
+            console.log("DESLOGADO");
+            var chaveLogado = "0";
+            localStorage.setItem("estaLogado", chaveLogado);
+            $("#loginCliente").hide();
+        };
+    };
+
+    verificaLogin();
+
     //Troca Div ENTRAR/CADASTRAR
     $("#formCadastrese").hide();
     $("#trocaDiv1").click(function () {
@@ -13,26 +35,25 @@ $(document).ready(function () {
     });
 
     /*
-        //Mascaras
-        $('#cpf').mask('999.999.999-99');
+    //Mascaras
+    $('#cpf').mask('999.999.999-99');
     
-        //Mascara RG
-        var mascaraRg = {
-            'translation': {
-                A: {
-                    pattern: /[A-Za-z0-9]/
-                }
-            }
-        };
-        $('#rg').mask('99.999.999-A' || '99.999.999-99', mascaraRg);
-        $('#telefone1').mask('(99)99999-9999');
-        $('#telefone2').mask('(99)99999-9999');
+    //Mascara RG
+    var mascaraRg = {
+       'translation': {
+           A: {
+               pattern: /[A-Za-z0-9]/
+           }
+       }
+    };
+    $('#rg').mask('99.999.999-A' || '99.999.999-99', mascaraRg);
+    $('#telefone1').mask('(99)99999-9999');
+    $('#telefone2').mask('(99)99999-9999');
     */
-    $(".loginCliente").hide();
 
-    var chaveLogado = 0;
+    // var chaveLogado = 0;
 
-    //Submit Login
+    //Submit Login (ENTRAR)
 
     $("#formLogin").on("submit", function (e) {
         e.preventDefault();
@@ -40,28 +61,12 @@ $(document).ready(function () {
         const formProps = Object.fromEntries(formData)
         localStorage.setItem("login", document.querySelector("#loginLogin").value);
         localStorage.setItem("senha", document.querySelector("#loginSenha").value);
-        const pegarLogin = localStorage.getItem("login");
-        const pegarSenha = localStorage.getItem("senha");
-
-        if (pegarLogin == "paulo muzzy" && pegarSenha == "123123") {
-            console.log("LOGADO");
-            var chaveLogado = "1";
-            localStorage.setItem("estaLogado", chaveLogado);
-            console.log("aqui s");
-        } else {
-            console.log("DESLOGADO");
-            var chaveLogado = "0";
-            localStorage.setItem("estaLogado", chaveLogado);
-            console.log("aqui n");
-        }
-
-        if (chaveLogado != "1") {
-            $(".loginCliente").hide();
-        } else {
-            $(".loginCliente").show();
-        }
-
+        verificaLogin();
     });
 
+    // DESLOGAR CRUD
+    $("#btSairLogin").on("click", function () {
+        localStorage.clear()
+        verificaLogin();
+    });
 });
-
