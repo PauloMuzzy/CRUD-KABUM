ab99f3bea475fbfd380801b93f89fb2fdfd91d4a
PauloMuzzy <paulokriger@gmail.com>
2021-11-17 14:50:36 -0300

    add passowrd_hash

--- www/api/cadastrarUser.php
+++ www/api/cadastrarUser.php
@@ -1,5 +1,7 @@
 <?php
 
+include('conexao.php');
+
 if ($_POST["acao"] == "inserir") {
 
     $myArray = $_POST['dados'];
@@ -8,18 +10,14 @@ if ($_POST["acao"] == "inserir") {
     $email = $myArray[1];
     $login = $myArray[2];
     $senha = $myArray[3];
+
+    $hashsenha = password_hash($senha, PASSWORD_DEFAULT);
+
     $typeuser = "0";
     $attcreat = "1";
     $attread = "0";
     $attupdate = "0";
     $attdelet = "0";
-
-    $servername = "db";
-    $database = "Logon";
-    $username = "root";
-    $password = "root";
-
-    include('conexao.php');
 
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
@@ -28,7 +26,7 @@ if ($_POST["acao"] == "inserir") {
     echo "Connected successfully";
 
     $sql = "INSERT INTO usuarios (nome, email, login, senha, typeuser, attcreat, attread, attupdate, attdelet) 
-    VALUES ('$nome', '$email', '$login', '$senha', '$typeuser', '$attcreat', '$attread', '$attupdate', '$attdelet')";
+    VALUES ('$nome', '$email', '$login', '$hashsenha', '$typeuser', '$attcreat', '$attread', '$attupdate', '$attdelet')";
     if (mysqli_query($conn, $sql)) {
         echo "New record created successfully";
     } else {
--- www/api/conexao.php
+++ www/api/conexao.php
@@ -1,3 +1,8 @@
 <?php
 
+$servername = "db";
+$database = "Logon";
+$username = "root";
+$password = "root";
+
 $conn = mysqli_connect($servername, $username, $password, $database);
