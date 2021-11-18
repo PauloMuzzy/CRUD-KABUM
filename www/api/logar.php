e3f9ef746eb5f83159ef4d79be5786d9f2ceaf24
PauloMuzzy <paulokriger@gmail.com>
2021-11-18 14:46:54 -0300

    Tela de login e cadastrar usuário

---
+++ www/api/logar.php
@@ -0,0 +1,43 @@
+<?php
+
+include('conexao.php');
+
+header('Content-Type: application/json');
+
+if ($_POST["acao"] == "logar") {
+
+    $arrayLoginSenha = $_POST['dados'];
+
+    $usuarioLogin = $arrayLoginSenha[0];
+    $usuarioSenha = $arrayLoginSenha[1];
+
+    $resultSenhaLogar = "SELECT senha FROM usuarios WHERE login = '$usuarioLogin'";
+    $resultadoSenhaLogar = mysqli_query($conn, $resultSenhaLogar);
+
+    while ($rows_usuarios = mysqli_fetch_array($resultadoSenhaLogar)) {
+        $usuarioSenhaHash = $rows_usuarios['senha'];
+    }
+
+    $resultNomeLogar = "SELECT nome FROM usuarios WHERE login = '$usuarioLogin'";
+    $resultadoNomeLogar = mysqli_query($conn, $resultNomeLogar);
+
+    while ($rows_usuarios = mysqli_fetch_array($resultadoNomeLogar)) {
+        $usuarioNome = $rows_usuarios['nome'];
+    }
+}
+
+if (password_verify($usuarioSenha, $usuarioSenhaHash)) {
+
+    $senhaVerificada = array(
+        "resultado" => "1",
+        "login" => "$usuarioLogin",
+        "nome" => "$usuarioNome"
+    );
+
+    echo json_encode($senhaVerificada);
+} else {
+    $senhaVerificada = array(
+        "resultado" => "0"
+    );
+    echo json_encode($senhaVerificada);
+}
--- www/scriptIndex.js
+++ www/scriptIndex.js
@@ -1,72 +1,113 @@
 $(document).ready(function () {
 
-    // Submit LOGAR
-    $(".formLogar").on("submit", function () {
-        var loginLogar = document.getElementById("loginLogar").value
-        var senhaLogar = document.getElementById("senhaLogar").value
+    $('.alertaSenhaIncorreta').hide()
+    $('.alertaSenhaCorreta').hide()
+    $('.alertaSenhasDiferentes').hide()
+    $('.cardCadastrese').hide()
+    $('.logado').hide()
 
-        console.log(loginLogar)
-        console.log(senhaLogar)
+    $("#btnCadastrese").click(function () {
+        $('.cardLogar').hide(120)
+        $('.cardCadastrese').show(120)
+    });
 
-        var arraySendPostLogar = new Array()
-        arraySendPostLogar[0] = loginLogar;
-        arraySendPostLogar[1] = senhaLogar;
+    $("#btnVoltar").click(function () {
+        $('.cardCadastrese').hide(120)
+        $('.cardLogar').show(120)
+    });
 
-        console.log(arraySendPostLogar)
+    // ------------------------------------- OK -----------------------------
+    // SUBMIT LOGAR
+    $(".formLogar").on("submit", function (e) {
+        e.preventDefault()
+        const loginLogar = document.getElementById("loginLogar").value
+        const senhaLogar = document.getElementById("senhaLogar").value
+
+        let arraySendPostLogar = new Array(loginLogar, senhaLogar)
 
         $.ajax({
             method: "POST",
             url: "/api/logar.php",
-            data: { dados: arraySendPostLogar, acao: "inserir" },
+            data: { dados: arraySendPostLogar, acao: "logar" },
             success: function (res) {
-                console.log(res)
+
+                const resultadoRes = res.resultado
+                const loginRes = res.login
+                const nomeRes = res.nome
+
+                $('.alertaSenhaIncorreta').hide()
+                $('.alertaSenhaCorreta').hide()
+
+                if (resultadoRes == "1") {
+                    localStorage.setItem("logado", resultadoRes)
+                    localStorage.setItem("login", loginRes)
+                    $('.alertaSenhaCorreta').show(150)
+                    $('.logado').show()
+                    const saudacao = "Olá, " + nomeRes + "."
+                    $(".nomeLogado").html(saudacao);
+
+                } else {
+                    $('.alertaSenhaIncorreta').show(150)
+                }
             }
         })
-
     })
 
-    // Submit CADASTRE-SE
-    $(".formCadastrese").on("submit", function () {
+    //EVENTO SAIR
+    $("#btnLoadoSair").click(function () {
+        localStorage.clear();
+        $('Form')[0].reset();
+        $('.alertaSenhaIncorreta').hide()
+        $('.alertaSenhaCorreta').hide()
+        $('.alertaSenhasDiferentes').hide()
+        $('.cardCadastrese').hide()
+        $('.logado').hide()
+    });
+
+    // ------------------------------------- OK -----------------------------
+    // SUBMIT CADASTRE-SE
+    $(".formCadastrese").on("submit", function (e) {
+        e.preventDefault()
         var nomeCadastrese = document.getElementById("nomeCadastrese").value
         var emailCadastrese = document.getElementById("emailCadastrese").value
         var loginCadastrese = document.getElementById("loginCadastrese").value
         var senhaCadastrese = document.getElementById("senhaCadastrese").value
-        // var confirmaSenhaCadastrese = document.getElementById("confimarSenhaCadastrese").value
+        var confirmaSenhaCadastrese = document.getElementById("confimarSenhaCadastrese").value
 
-        var arraySendPostCadastrese = new Array()
-        arraySendPostCadastrese[0] = nomeCadastrese;
-        arraySendPostCadastrese[1] = emailCadastrese;
-        arraySendPostCadastrese[2] = loginCadastrese;
-        arraySendPostCadastrese[3] = senhaCadastrese;
-        //arraySendPostCadastrese[4] = confirmaSenhaCadastrese;
+        if (senhaCadastrese == confirmaSenhaCadastrese) {
+            var arraySendPostCadastrese = new Array()
+            arraySendPostCadastrese[0] = nomeCadastrese;
+            arraySendPostCadastrese[1] = emailCadastrese;
+            arraySendPostCadastrese[2] = loginCadastrese;
+            arraySendPostCadastrese[3] = senhaCadastrese;
 
-
-        $.ajax({
-            method: "POST",
-            url: "/api/cadastrarUser.php",
-            data: { dados: arraySendPostCadastrese, acao: "inserir" },
-            success: function (res) {
-                console.log(res)
-            }
-        })
-
+            $.ajax({
+                method: "POST",
+                url: "/api/cadastrarUser.php",
+                data: { dados: arraySendPostCadastrese, acao: "inserir" },
+                success: function (res) {
+                    console.log(res)
+                }
+            })
+        } else {
+            $('.alertaSenhasDiferentes').show()
+        }
     })
 
-})
-
-/*
-//Mascaras
-$('#cpf').mask('999.999.999-99');
-
-//Mascara RG
-var mascaraRg = {
-'translation': {
-    A: {
-        pattern: /[A-Za-z0-9]/
+    /*
+    //Mascaras
+    $('#cpf').mask('999.999.999-99');
+    
+    //Mascara RG
+    var mascaraRg = {
+    'translation': {
+        A: {
+            pattern: /[A-Za-z0-9]/
+        }
     }
-}
-};
-$('#rg').mask('99.999.999-A' || '99.999.999-99', mascaraRg);
-$('#telefone1').mask('(99)99999-9999');
-$('#telefone2').mask('(99)99999-9999');
-*/
+    };
+    $('#rg').mask('99.999.999-A' || '99.999.999-99', mascaraRg);
+    $('#telefone1').mask('(99)99999-9999');
+    $('#telefone2').mask('(99)99999-9999');
+    */
+})
