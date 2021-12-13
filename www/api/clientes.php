e2c48f605a83af65d77f46d97b368ae9ba88fc26
PauloMuzzy <paulokriger@gmail.com>
2021-12-13 07:21:16 -0300

    Organização de pastas

--- www/api/clientes.php
+++ www/api/clientes.php
@@ -3,20 +3,22 @@
 include('conexao.php');
 
 header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+
+// ################################### POST ################################### 
 
 if ($_POST) {
 
     if ($_POST['acao'] == 'cadastraCliente') {
 
         $nome =  $_POST['nome'];
-        $cpf = intval($_POST['cpf']);
+        $cpf = $_POST['cpf'];
         $rg =  $_POST['rg'];
         $email = $_POST['email'];
-        $tel1 = intval($_POST['tel1']);
-        $tel2 = intval($_POST['tel2']);
+        $tel1 = $_POST['tel1'];
+        $tel2 = $_POST['tel2'];
         $dataNasc = $_POST['dataNasc'];
-        $idUsuario = intval($_POST['idUsuario']);
-        $ativo = 1;
+        $idUsuario = $_POST['idUsuario'];
 
         try {
             $query = $pdo->prepare('INSERT INTO clientes (nome,cpf,rg,email,telefone1,telefone2,data_nasc,id_usuario,ativo) 
@@ -36,23 +38,23 @@ if ($_POST) {
         } catch (PDOException $e) {
             echo 'Error: ' . $e->getMessage();
         }
-    } else if ($_POST['acao'] == 'cadastraEnderecos') {
+    } else if ($_POST['acao'] == 'cadastraEndereco') {
 
-        $cpf_cliente = intval($_POST['cpf']);
+        $cpf_cliente = $_POST['cpf'];
         $rua = $_POST['rua'];
         $numero = $_POST['numero'];
         $bairro = $_POST['bairro'];
-        $cep = intval($_POST['cep']);
+        $cep = $_POST['cep'];
         $cidade = $_POST['cidade'];
         $estado = $_POST['estado'];
-        $principal = intval($_POST['principal']);
-        if ($principal == 1) {
-            $principal = 1;
+        $enderecoOrdem = $_POST['enderecoOrdem'];
+        if ($enderecoOrdem == 1) {
+            $enderecoOrdem = 1;
         } else {
-            $principal = 0;
+            $enderecoOrdem = 0;
         }
-        $ativo = intval($_POST['ativo']);
-        $id_usuario = intval($_POST['idUsuario']);
+        $ativo = 1;
+        $id_usuario = $_POST['idUsuario'];
 
         try {
             $query = $pdo->prepare('INSERT INTO enderecos (cpf_cliente,rua,numero,bairro,cep,cidade,estado,principal,ativo,id_usuario) 
@@ -65,7 +67,7 @@ if ($_POST) {
             $query->bindParam(5, $cep);
             $query->bindParam(6, $cidade);
             $query->bindParam(7, $estado);
-            $query->bindParam(8, $principal);
+            $query->bindParam(8, $enderecoOrdem);
             $query->bindParam(9, $ativo);
             $query->bindParam(10, $id_usuario);
 
@@ -75,6 +77,8 @@ if ($_POST) {
         }
     }
 }
+
+// ################################### GET ################################### 
 
 if ($_GET) {
     if ($_GET['acao'] == 'listarClientes') {
@@ -105,5 +109,61 @@ if ($_GET) {
         } catch (PDOException $e) {
             echo 'Error: ' . $e->getMessage();
         }
+    }
+
+    if ($_GET['acao'] == 'listarUpdateCliente') {
+
+        $id = $_GET['id'];
+
+        try {
+            $query = $pdo->prepare('SELECT id_cliente,nome,cpf,rg,email,telefone1,telefone2,data_nasc,id_usuario,ativo FROM clientes WHERE ativo =1 AND id_cliente =?');
+
+            $query->bindParam(1, $id);
+
+            $query->execute();
+
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+            echo json_encode($result);
+        } catch (PDOException $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
+    }
+}
+
+// ################################### PUT ################################### 
+
+if ($method === 'PUT') {
+    parse_str(file_get_contents('php://input'), $_PUT);
+
+    if ($_PUT['acao'] == "updateCliente") {
+
+        $idCliente = $_PUT['idCliente'];
+        $nome = $_PUT['nome'];
+        $cpf = $_PUT['cpf'];
+        $rg = $_PUT['rg'];
+        $email = $_PUT['email'];
+        $telefone1 = $_PUT['telefone1'];
+        $telefone2 = $_PUT['telefone2'];
+        $dataNasc = $_PUT['dataNasc'];
+
+        try {
+            $query = $pdo->prepare('UPDATE clientes SET nome=?,cpf=?,rg=?,email=?,telefone1=?,telefone2=?,data_nasc=? WHERE id_cliente=?');
+
+            $query->bindParam(1, $nome);
+            $query->bindParam(2, $cpf);
+            $query->bindParam(3, $rg);
+            $query->bindParam(4, $email);
+            $query->bindParam(5, $telefone1);
+            $query->bindParam(6, $telefone2);
+            $query->bindParam(7, $dataNasc);
+            $query->bindParam(8, $idCliente);
+
+            $query->execute();
+        } catch (PDOException $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
+
+        echo json_encode($_PUT);
     }
 }
---
+++ www/clientes/create/clientecreate.js
@@ -0,0 +1,198 @@
+$(function () {
+
+    //Mascaras
+    $('.cpf').mask('999.999.999-99');
+    $('.telefone').mask('(99)99999-9999');
+    $('.telefone').mask('(99)99999-9999');
+    $('.cep').mask('99999-999')
+
+    var mascaraRg = {
+        'translation': {
+            A: {
+                pattern: /[A-Za-z0-9]/
+            }
+        }
+    };
+    $('.rg').mask('99.999.999-A' || '99.999.999-99', mascaraRg);
+
+
+    // ------------------------------------- OK -----------------------------
+    //ADICIONA FORMULÁRIO DE ENDEREÇO
+    var clicks = 0;
+    $('#botaoAdicionaEndereco').click(function (e) {
+        e.preventDefault()
+
+        clicks += 1;
+
+        var tagForm = document.createElement('form')
+        $('#divEnderecos').append(tagForm)
+        tagForm.setAttribute('id', 'formularioEnderecos' + clicks)
+        tagForm.setAttribute('method', 'POST')
+        tagForm.setAttribute('class', 'formularioEnderecos')
+
+        var tagH3 = document.createElement('h3')
+        tagH3.setAttribute('id', 'h3Enderecos' + clicks)
+        tagForm.appendChild(tagH3)
+
+        if (clicks == '1') {
+            tagH3.textContent = 'ENDEREÇO ' + clicks + ' - (PRINCIPAL)'
+        } else {
+            tagH3.textContent = 'ENDEREÇO ' + clicks
+
+        }
+
+        // ----------- RUA -----------
+
+        var tagLabel = document.createElement('label')
+        tagLabel.setAttribute('for', 'ruaEndereco' + clicks)
+        tagForm.appendChild(tagLabel)
+        tagLabel.textContent = 'Rua/Avenida'
+
+        var tagInput = document.createElement('input')
+        tagInput.setAttribute('name', 'ruaEndereco')
+        tagInput.setAttribute('id', 'ruaEndereco' + clicks)
+        tagInput.setAttribute('style', 'text-transform:uppercase')
+        tagForm.appendChild(tagInput)
+
+        // ----------- NÚMERO -----------
+
+        var tagLabel = document.createElement('label')
+        tagLabel.setAttribute('for', 'numeroEndereco' + clicks)
+        tagForm.appendChild(tagLabel)
+        tagLabel.textContent = 'Número'
+
+        var tagInput = document.createElement('input')
+        tagInput.setAttribute('name', 'numeroEndereco')
+        tagInput.setAttribute('id', 'numeroEndereco' + clicks)
+        tagInput.setAttribute('style', 'text-transform:uppercase')
+        tagForm.appendChild(tagInput)
+
+        // ----------- BAIRRO -----------
+
+        var tagLabel = document.createElement('label')
+        tagLabel.setAttribute('for', 'bairroEndereco' + clicks)
+        tagForm.appendChild(tagLabel)
+        tagLabel.textContent = 'Bairro'
+
+        var tagInput = document.createElement('input')
+        tagInput.setAttribute('name', 'bairroEndereco')
+        tagInput.setAttribute('id', 'bairroEndereco' + clicks)
+        tagInput.setAttribute('style', 'text-transform:uppercase')
+        tagForm.appendChild(tagInput)
+
+        // ----------- CEP -----------
+
+        var tagLabel = document.createElement('label')
+        tagLabel.setAttribute('for', 'cepEndereco' + clicks)
+        tagForm.appendChild(tagLabel)
+        tagLabel.textContent = 'CEP'
+
+        var tagInput = document.createElement('input')
+        tagInput.setAttribute('class', 'cep')
+        tagInput.setAttribute('name', 'cepEndereco')
+        tagInput.setAttribute('id', 'cepEndereco' + clicks)
+        tagForm.appendChild(tagInput)
+        $('.cep').mask('99999-999')
+
+        // ----------- CIDADE -----------
+
+        var tagLabel = document.createElement('label')
+        tagLabel.setAttribute('for', 'cidadeEndereco' + clicks)
+        tagForm.appendChild(tagLabel)
+        tagLabel.textContent = 'Cidade'
+
+        var tagInput = document.createElement('input')
+        tagInput.setAttribute('name', 'cidadeEndereco')
+        tagInput.setAttribute('id', 'cidadeEndereco' + clicks)
+        tagInput.setAttribute('style', 'text-transform:uppercase')
+        tagForm.appendChild(tagInput)
+
+        // ----------- ESTADO -----------
+
+        var tagLabel = document.createElement('label')
+        tagLabel.setAttribute('for', 'estadoEndereco' + clicks)
+        tagForm.appendChild(tagLabel)
+        tagLabel.textContent = 'Estado'
+
+        var tagInput = document.createElement('input')
+        tagInput.setAttribute('name', 'estadoEndereco')
+        tagInput.setAttribute('id', 'estadoEndereco' + clicks)
+        tagInput.setAttribute('style', 'text-transform:uppercase')
+        tagForm.appendChild(tagInput)
+    })
+
+
+
+    // ------------------------------------- OK -----------------------------
+    //CADASTRA CLIENTE E ENDREÇO(S)
+    $('#botaoCadastraCliente').click(function (e) {
+        e.preventDefault()
+
+        // const confirmaCadastro = confirm('Confirma cadastro do cliente?')
+
+
+        // (confirmaCadastro == true) {
+        const nomeCadastraCliente = document.getElementById('nomeCadastraCliente').value
+        const cpfCadastraCliente = document.getElementById('cpfCadastraCliente').value
+        const rgCadastraCliente = document.getElementById('rgCadastraCliente').value
+        const emailCadastraCliente = document.getElementById('emailCadastraCliente').value
+        const tel1CadastraCliente = document.getElementById('tel01CadastraCliente').value
+        const tel2CadastraCliente = document.getElementById('tel02CadastraCliente').value
+        const dataNascCadastraCliente = document.getElementById('dataNascCadastraCliente').value
+
+        const objCadastraCliente = {
+            acao: 'cadastraCliente',
+            nome: nomeCadastraCliente,
+            cpf: cpfCadastraCliente,
+            rg: rgCadastraCliente,
+            email: emailCadastraCliente,
+            tel1: tel1CadastraCliente,
+            tel2: tel2CadastraCliente,
+            dataNasc: dataNascCadastraCliente,
+            idUsuario: idUsuario
+        }
+
+        // $.ajax({
+        //     method: 'POST',
+        //     url: '/api/clientes.php',
+        //     data: objCadastraCliente,
+        //     success: function (res) {
+        //         console.log(res)
+        //     }
+        // })
+
+        for (var i = 1; i <= clicks; i++) {
+            const ruaEndereco = document.getElementById('ruaEndereco' + i).value
+            const numeroEndereco = document.getElementById('numeroEndereco' + i).value
+            const bairroEndereco = document.getElementById('bairroEndereco' + i).value
+            const cepEndereco = document.getElementById('cepEndereco' + i).value
+            const cidadeEndereco = document.getElementById('cidadeEndereco' + i).value
+            const estadoEndereco = document.getElementById('estadoEndereco' + i).value
+
+            const objFirstEndereco = {
+                acao: 'cadastraEndereco',
+                cpf: cpfCadastraCliente,
+                rua: ruaEndereco,
+                numero: numeroEndereco,
+                bairro: bairroEndereco,
+                cep: cepEndereco,
+                cidade: cidadeEndereco,
+                estado: estadoEndereco,
+                enderecoOrdem: i,
+                idUsuario: idUsuario
+            }
+
+            console.log(objFirstEndereco)
+
+            // $.ajax({
+            //     method: 'POST',
+            //     url: '/api/clientes.php',
+            //     data: objFirstEndereco,
+            //     success: function (res) {
+            //         console.log(res)
+            //     }
+            // })
+
+        }
+    })
+})
